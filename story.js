60993d2c39590e201ace046f2de2fe2ece5a203e
Yousif AlOzairi <yoyo012@icloud.com>
2020-06-15 17:34:28 +0300

    solved the task

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+let name = "Yoyo";
 // Define the age here on line 4
-
+let age = 16;
 // Define the hobby here on line 6
-
+let hobby = "programming";
 // Print the story here on line 8
+console.log(
+  `Mr. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
